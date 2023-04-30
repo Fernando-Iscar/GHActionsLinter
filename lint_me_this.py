68bcf5845cc579d1b1e900335b7df79f7c9095a9
Fernando Iscar <85165578+Fernando-Iscar@users.noreply.github.com>
2023-04-30 17:54:32 +0200

    Create lint_me_this.py

---
+++ lint_me_this.py
@@ -0,0 +1,11 @@
+#This is not a proper linted format:
+
+def saygoodmorning():
+                print("Good morning Fernando!")
+
+# This IS a proper linted format:
+
+def hello():
+  print("Hello Fernando!")
+  
+# So after running the workflow, Super-Linter should advice of two errors
